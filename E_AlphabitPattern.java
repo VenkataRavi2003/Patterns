526068499d5a6850bcbff07030fb00e7c445ffbc
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-26 13:31:39 +0530

    Add files via upload

---
+++ E_AlphabitPattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class E_AlphabitPattern{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter n : ");
+
+		int n = sc.nextInt();
+
+		print_E_Pattern(n);
+
+		sc.close();
+	}
+	public static void print_E_Pattern(int n){
+
+		for (int i = 0; i < n ; ++i ) {
+
+			for (int j = 0 ; j < n ; ++j ) {
+
+				if((j == 0  && i != 0 && i != n-1)||(i == 0 && j != 0 || i == n-1 && j != 0)||(i == n/2)){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+			
+		}
+	}
+}
