222acddc11448267eacd15fcd2accc1fed1971a5
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-26 17:34:48 +0530

    Add files via upload

---
+++ Z_AlphabitPattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Z_AlphabitPattern{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter n : ");
+
+		int n = sc.nextInt();
+
+		print_Z_Pattern(n);
+
+		sc.close();
+	}
+	public static void print_Z_Pattern(int n){
+
+		for (int i = 0; i < n ; ++i ) {
+
+			for (int j = 0 ; j < n ; ++j ) {
+
+				if(i == 0 || i == n-1 || i+j == n-1){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+			
+		}
+	}
+}
