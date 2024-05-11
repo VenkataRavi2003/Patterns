8acf9ffde5683f20c160a2f6906fb49fc761c3b2
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-05-11 16:05:43 +0530

    Update and rename InvertedRight_LeftAngleTriangles.java to InvertedRightAngleTriangle.java

--- InvertedRightAngleTriangle.java
+++ InvertedRightAngleTriangle.java
@@ -1,20 +1,12 @@
 import java.util.Scanner;
-class InvertedRight_LeftAngleTriangles{
+public class InvertedRightAngleTriangle{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		System.out.print("\nEnter the number : ");
 		int n=sc.nextInt();
-		System.out.println("\nInverted Left & Right Angle Triangles");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("\nInverted Right Angle Triangle");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		for(int i=0;i<n;++i){
-			for(int j=0;j<n;++j){
-				if(i==0||j==0||i-j==n-1){
-					System.out.print("*");
-				}else{
-					System.out.print(" ");
-				}
-			}
-			System.out.print(" ");
 			for(int j=0;j<n;++j){
 				if(i==0||j==n-1||i==j){
 					System.out.print("*");
@@ -24,5 +16,6 @@ class InvertedRight_LeftAngleTriangles{
 			}
 			System.out.println();
 		}
+		sc.close();
 	}
 }
